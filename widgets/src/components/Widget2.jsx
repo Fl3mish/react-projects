ca5db64758cdb44fa3167179e28338d601122f50
Fl3mish <mitch.pauwels@proton.me>
2024-09-06 00:35:29 +0100

    Widget2 Complete

--- widgets/src/components/Widget2.jsx
+++ widgets/src/components/Widget2.jsx
@@ -1,7 +1,19 @@
+import { useState } from "react";
+
 const Widget2 = () => {
+  const [toggleColor, setToggleColor] = useState(false);
   return (
-    <div className="bg-blue-100/60 border border-blue-300 rounded-md p-8 my-4">
-      <button className="bg-blue-500 text-white px-4 py-2 rounded-full">
+    <div
+      className={`${
+        toggleColor ? "bg-orange-100 border-orange-300" : "bg-sky-100"
+      } border border-sky-300 rounded-md p-8 my-4`}
+    >
+      <button
+        onClick={() => setToggleColor(!toggleColor)}
+        className={`${
+          toggleColor ? "bg-orange-500" : "bg-blue-500"
+        } text-white px-4 py-2 rounded-full`}
+      >
         click me
       </button>
     </div>
