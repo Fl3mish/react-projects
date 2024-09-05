c6a07a1186711df775626b3e838e9d4f4881ccee
Fl3mish <mitch.pauwels@proton.me>
2024-09-05 22:55:38 +0100

    Finished

--- click-event/src/App.jsx
+++ click-event/src/App.jsx
@@ -1,3 +1,30 @@
+import { useState } from "react";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Skeleton</h1>;
+  const [message, setMessage] = useState("Hola!");
+  return (
+    <div className="flex flex-col justify-center items-center h-screen">
+      <div className="mb-4 text-stone-400 text-3xl">{message}</div>
+      <div>
+        <button
+          onClick={() => setMessage("Uno")}
+          className="px-2 py-1 bg-cyan-600 mx-2 text-white rounded-md"
+        >
+          one
+        </button>
+        <button
+          onClick={() => setMessage("Dos")}
+          className="px-2 py-1 bg-violet-600 mx-2 text-white rounded-md"
+        >
+          two
+        </button>
+        <button
+          onClick={() => setMessage("Tres")}
+          className="px-2 py-1 bg-yellow-500 mx-2 text-white rounded-md"
+        >
+          three
+        </button>
+      </div>
+    </div>
+  );
 }
