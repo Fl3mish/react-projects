3d99b56d4e05040eb830d02f3dda129e8163bbd0
Fl3mish <mitch.pauwels@proton.me>
2024-09-06 00:41:02 +0100

    Widget3 complete

--- widgets/src/components/Widget3.jsx
+++ widgets/src/components/Widget3.jsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
+
 const Widget3 = () => {
+  const [showIcon, setShowIcon] = useState(false);
   return (
-    <div className="bg-violet-100/60 border border-violet-300 rounded-md p-8 my-4">
-      <button className="bg-violet-500 text-white px-4 py-2 rounded-full">
+    <div className="flex flex-col items-center bg-violet-100/60 border border-violet-300 rounded-md p-8 my-4">
+      {showIcon && (
+        <div className="mb-4">
+          <i className="fa-solid fa-poop text-violet-500 text-3xl"></i>
+        </div>
+      )}
+      <button
+        onClick={() => setShowIcon(!showIcon)}
+        className="bg-violet-500 text-white px-4 py-2 rounded-full"
+      >
         click me
       </button>
     </div>
