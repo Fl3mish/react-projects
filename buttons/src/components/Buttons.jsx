f1280911a48cfb372f6001b2a089781c7b5be119
Fl3mish <mitch.pauwels@proton.me>
2024-09-05 13:28:13 +0100

    Few modifications to the style

--- buttons/src/components/Buttons.jsx
+++ buttons/src/components/Buttons.jsx
@@ -1,16 +1,16 @@
 const Buttons = () => {
   return (
     <div>
-      <button className="border-2 text-green-700 border-green-700 px-8 py-2 rounded-lg m-4">
+      <button className="border-2 text-green-400 border-green-600 px-8 py-2 rounded-md m-4">
         first
       </button>
-      <button className="border-2 text-white bg-blue-400 border-blue-400 px-16 py-4 rounded-sm m-4">
+      <button className=" text-white text-3xl bg-blue-400 px-10 py-4 rounded-lg m-4 hover:text-base">
         second
       </button>
-      <button className="border-2 text-purple-600 font-semibold bg-purple-100 border-purple-100 px-6 py-1 rounded-3xl m-4">
+      <button className=" text-violet-600 font-semibold bg-violet-100 px-8 py-2 rounded-full m-4">
         third
       </button>
-      <button className="border-2 text-white/70 font-semibold bg-sky-700 border-sky-700 px-8 py-9 rounded-md m-4">
+      <button className=" text-cyan-200 font-semibold bg-cyan-700 p-10 rounded-lg m-4 hover:bg-cyan-200 hover:text-cyan-700">
         fourth
       </button>
     </div>
