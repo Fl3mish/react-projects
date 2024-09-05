5fd76ad7b917966fea8b6311569dcc253881f6b7
Fl3mish <mitch.pauwels@proton.me>
2024-09-05 13:17:48 +0100

    Build 4 different buttons

--- buttons/src/App.jsx
+++ buttons/src/App.jsx
@@ -1,3 +1,9 @@
+import Buttons from "./components/Buttons";
+
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Skeleton</h1>;
+  return (
+    <>
+      <Buttons />
+    </>
+  );
 }
---
+++ buttons/src/components/Buttons.jsx
@@ -0,0 +1,19 @@
+const Buttons = () => {
+  return (
+    <div>
+      <button className="border-2 text-green-700 border-green-700 px-8 py-2 rounded-lg m-4">
+        first
+      </button>
+      <button className="border-2 text-white bg-blue-400 border-blue-400 px-16 py-4 rounded-sm m-4">
+        second
+      </button>
+      <button className="border-2 text-purple-600 font-semibold bg-purple-100 border-purple-100 px-6 py-1 rounded-3xl m-4">
+        third
+      </button>
+      <button className="border-2 text-white/70 font-semibold bg-sky-700 border-sky-700 px-8 py-9 rounded-md m-4">
+        fourth
+      </button>
+    </div>
+  );
+};
+export default Buttons;
