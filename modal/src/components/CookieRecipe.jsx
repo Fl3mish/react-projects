2594eef0e49e328b042bdc7dee0d720a074ab021
Fl3mish <mitch.pauwels@proton.me>
2024-09-06 19:31:36 +0100

    content added

--- modal/src/components/CookieRecipe.jsx
+++ modal/src/components/CookieRecipe.jsx
@@ -1,4 +1,51 @@
 const CookieRecipe = () => {
-  return <div>CookieRecipe</div>;
+  return (
+    <div className="flex flex-col items-center p-4">
+      <div className="w-full max-w-3xl">
+        <h2 className="my-6 text-3xl">Classic Chocolate Chip Cookies </h2>
+
+        <p className="my-4 leading-relaxed">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore optio
+          numquam necessitatibus. Eligendi, corrupti labore, odit rem ex minima,
+          sequi blanditiis ratione pariatur id eos? Officia aspernatur quidem
+          ipsum consequatur. Impedit deleniti consequuntur quis, obcaecati eos
+          placeat. Explicabo dolorem tempore ipsa architecto, vitae saepe
+          quibusdam repudiandae mollitia laboriosam nobis quae, adipisci,
+          inventore eligendi harum ratione! Cumque voluptatibus debitis ab
+          nostrum?
+        </p>
+        <p className="my-4 leading-relaxed">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore optio
+          numquam necessitatibus. Eligendi, corrupti labore, odit rem ex minima,
+          sequi blanditiis ratione pariatur id eos? Officia aspernatur quidem
+          ipsum consequatur. Impedit deleniti consequuntur quis, obcaecati eos
+          placeat. Explicabo dolorem tempore ipsa architecto, vitae saepe
+          quibusdam repudiandae mollitia laboriosam nobis quae, adipisci,
+          inventore eligendi harum ratione! Cumque voluptatibus debitis ab
+          nostrum?
+        </p>
+        <p className="my-4 leading-relaxed">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore optio
+          numquam necessitatibus. Eligendi, corrupti labore, odit rem ex minima,
+          sequi blanditiis ratione pariatur id eos? Officia aspernatur quidem
+          ipsum consequatur. Impedit deleniti consequuntur quis, obcaecati eos
+          placeat. Explicabo dolorem tempore ipsa architecto, vitae saepe
+          quibusdam repudiandae mollitia laboriosam nobis quae, adipisci,
+          inventore eligendi harum ratione! Cumque voluptatibus debitis ab
+          nostrum?
+        </p>
+        <p className="my-4 leading-relaxed">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore optio
+          numquam necessitatibus. Eligendi, corrupti labore, odit rem ex minima,
+          sequi blanditiis ratione pariatur id eos? Officia aspernatur quidem
+          ipsum consequatur. Impedit deleniti consequuntur quis, obcaecati eos
+          placeat. Explicabo dolorem tempore ipsa architecto, vitae saepe
+          quibusdam repudiandae mollitia laboriosam nobis quae, adipisci,
+          inventore eligendi harum ratione! Cumque voluptatibus debitis ab
+          nostrum?
+        </p>
+      </div>
+    </div>
+  );
 };
 export default CookieRecipe;
