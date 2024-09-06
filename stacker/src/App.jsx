c032754adefcc808ffb4a6b61d1ba36e06efffcd
Fl3mish <mitch.pauwels@proton.me>
2024-09-06 15:22:44 +0100

    Hardcoded UI

--- stacker/src/App.jsx
+++ stacker/src/App.jsx
@@ -1,3 +1,20 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Skeleton</h1>;
-}
+const App = () => {
+  return (
+    <div className="flex flex-col items-center justify-end h-screen  border-8 border-red-900">
+      <div className="p-2 border-b-4 border-neutral-500">
+        <div className="bg-green-400 w-28 h-8 mb-2 rounded-sm"></div>
+        <div className="bg-green-400 w-28 h-8 mb-2 rounded-sm"></div>
+      </div>
+
+      <div className="flex">
+        <button className="bg-yellow-200 text-yellow-600 m-4 p-2 rounded-lg">
+          remove block
+        </button>
+        <button className="bg-sky-200 text-sky-500 m-4 p-2 rounded-lg">
+          add block
+        </button>
+      </div>
+    </div>
+  );
+};
+export default App;
