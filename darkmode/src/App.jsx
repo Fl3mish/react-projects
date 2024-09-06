9692d4f54e161d3a06edd416bf5135e681b37261
Fl3mish <mitch.pauwels@proton.me>
2024-09-06 18:38:05 +0100

    responsive text

--- darkmode/src/App.jsx
+++ darkmode/src/App.jsx
@@ -1,3 +1,45 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Skeleton</h1>;
-}
+const App = () => {
+  return (
+    <div className="flex justify-center p-4">
+      <div className=" w-full max-w-2xl">
+        <p className="my-4">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex vero, in
+          sit laudantium quisquam vel ab perspiciatis aspernatur corrupti hic
+          saepe quaerat, enim vitae soluta delectus qui sed quis animi.
+          Consequatur cumque illo perferendis provident vitae, culpa fugiat
+          maiores dolore eos iste omnis, aspernatur, assumenda deleniti?
+          Voluptatum veniam ut, placeat nisi odio, repellendus illo animi fuga
+          sed dolorum dolore ab.
+        </p>
+        <p className="my-4">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex vero, in
+          sit laudantium quisquam vel ab perspiciatis aspernatur corrupti hic
+          saepe quaerat, enim vitae soluta delectus qui sed quis animi.
+          Consequatur cumque illo perferendis provident vitae, culpa fugiat
+          maiores dolore eos iste omnis, aspernatur, assumenda deleniti?
+          Voluptatum veniam ut, placeat nisi odio, repellendus illo animi fuga
+          sed dolorum dolore ab.
+        </p>
+        <p className="my-4">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex vero, in
+          sit laudantium quisquam vel ab perspiciatis aspernatur corrupti hic
+          saepe quaerat, enim vitae soluta delectus qui sed quis animi.
+          Consequatur cumque illo perferendis provident vitae, culpa fugiat
+          maiores dolore eos iste omnis, aspernatur, assumenda deleniti?
+          Voluptatum veniam ut, placeat nisi odio, repellendus illo animi fuga
+          sed dolorum dolore ab.
+        </p>
+        <p className="my-4">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex vero, in
+          sit laudantium quisquam vel ab perspiciatis aspernatur corrupti hic
+          saepe quaerat, enim vitae soluta delectus qui sed quis animi.
+          Consequatur cumque illo perferendis provident vitae, culpa fugiat
+          maiores dolore eos iste omnis, aspernatur, assumenda deleniti?
+          Voluptatum veniam ut, placeat nisi odio, repellendus illo animi fuga
+          sed dolorum dolore ab.
+        </p>
+      </div>
+    </div>
+  );
+};
+export default App;
