a5983e0c301c6ab7bfcf4ae7ac0abb2fdfb652a8
Fl3mish <mitch.pauwels@proton.me>
2024-09-07 15:47:33 +0100

    init commit

---
+++ songs/src/songs.jsx
@@ -0,0 +1,64 @@
+const songs = [
+  {
+    title: "Blinding Lights",
+    artist: "The Weeknd",
+    album: "After Hours",
+    duration: 200,
+  },
+  {
+    title: "Levitating",
+    artist: "Dua Lipa",
+    album: "Future Nostalgia",
+    duration: 203,
+  },
+  {
+    title: "Peaches",
+    artist: "Justin Bieber",
+    album: "Justice",
+    duration: 198,
+  },
+  {
+    title: "Watermelon Sugar",
+    artist: "Harry Styles",
+    album: "Fine Line",
+    duration: 174,
+  },
+  {
+    title: "Save Your Tears",
+    artist: "The Weeknd",
+    album: "After Hours",
+    duration: 215,
+  },
+  {
+    title: "Good 4 U",
+    artist: "Olivia Rodrigo",
+    album: "SOUR",
+    duration: 178,
+  },
+  {
+    title: "Montero (Call Me By Your Name)",
+    artist: "Lil Nas X",
+    album: "Montero",
+    duration: 137,
+  },
+  {
+    title: "Drivers License",
+    artist: "Olivia Rodrigo",
+    album: "SOUR",
+    duration: 242,
+  },
+  {
+    title: "Kiss Me More",
+    artist: "Doja Cat",
+    album: "Planet Her",
+    duration: 208,
+  },
+  {
+    title: "Stay",
+    artist: "The Kid LAROI & Justin Bieber",
+    album: "F*CK LOVE 3: OVER YOU",
+    duration: 141,
+  },
+];
+
+export default songs;
