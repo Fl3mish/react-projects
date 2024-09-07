5a4e9463f9bccb4e53caf93cf9a1da9ba01c09f4
Fl3mish <mitch.pauwels@proton.me>
2024-09-07 10:24:53 +0100

    Build Header

--- modal/src/App.jsx
+++ modal/src/App.jsx
@@ -2,9 +2,18 @@ import CookieRecipe from "./components/CookieRecipe";
 
 const App = () => {
   return (
-    <div>
+    <>
+      <header className="flex justify-center py-4 bg-purple-700 text-purple-200">
+        <div className="w-full max-w-4xl flex justify-between px-4">
+          <div className=" text-2xl">Mitch&apos;s Recipes</div>
+          <div className=" text-lg">
+            <i className="fa-solid fa-right-to-bracket p-2"></i>
+            <button>Sign-In</button>
+          </div>
+        </div>
+      </header>
       <CookieRecipe />
-    </div>
+    </>
   );
 };
 export default App;
