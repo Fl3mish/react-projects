df63ac63c27b943f492be500df88aae04f9f4860
Fl3mish <mitch.pauwels@proton.me>
2024-09-07 15:26:29 +0100

    completed messages

--- map-exercise/src/App.jsx
+++ map-exercise/src/App.jsx
@@ -1,4 +1,18 @@
+import messages from "./messages";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="flex flex-col items-center p-4 w-full">
+      {messages.map((message) => (
+        <div
+          key={message.text}
+          className="flex p-2 border border-slate-500 rounded-lg my-2 w-full max-w-xl"
+        >
+          <div className="text-slate-800 font-bold mr-2">{message.user}</div>{" "}
+          <div>{message.text}</div>
+        </div>
+      ))}
+    </div>
+  );
 };
 export default App;
