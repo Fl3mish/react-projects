c835d48ea8c2e947a428c1c32c97c58ddff5aefd
Fl3mish <mitch.pauwels@proton.me>
2024-09-10 00:04:56 +0100

    Finished

--- use-effect-exercise/src/App.jsx
+++ use-effect-exercise/src/App.jsx
@@ -1,3 +1,39 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Skeleton</h1>;
-}
+import { useEffect, useState } from "react";
+import Joke from "./Joke";
+
+const App = () => {
+  const [showAnswer, setShowAnswer] = useState(false);
+  const [joke, setJoke] = useState(null);
+
+  const fetchNewJoke = () => {
+    setShowAnswer(false);
+    fetch("https://api.react-formula.com/learning-api/demos/random-joke/jokes")
+      .then((response) => response.json())
+      .then((data) => setJoke(data));
+  };
+
+  useEffect(() => {
+    fetchNewJoke();
+  }, []);
+
+  return (
+    <div className="flex flex-col items-center py-16">
+      <Joke joke={joke} showAnswer={showAnswer} />
+      <button
+        onClick={() => setShowAnswer(!showAnswer)}
+        className=" px-4 py-2 m-6  bg-sky-600 text-sky-50 rounded-lg"
+      >
+        Show Answer
+      </button>
+      <button
+        onClick={() => {
+          fetchNewJoke();
+        }}
+        className=" px-4 py-2  m-6 bg-violet-600 text-violet-50 rounded-lg"
+      >
+        New Joke
+      </button>
+    </div>
+  );
+};
+export default App;
