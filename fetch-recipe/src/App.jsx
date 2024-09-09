75a0c3f9b043443a44378d05d1fe3fb954ccfbf3
Fl3mish <mitch.pauwels@proton.me>
2024-09-09 21:51:35 +0100

    Build Fetch Recipe

--- fetch-recipe/src/App.jsx
+++ fetch-recipe/src/App.jsx
@@ -1,16 +1,23 @@
+import { useState } from "react";
 import RecipeCard from "./RecipeCard";
 
-const recipeCard = {
-  name: "Hamburger and Fries",
-  imageUrl:
-    "https://images.pexels.com/photos/1556688/pexels-photo-1556688.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-};
-
 const App = () => {
+  const [recipe, setRecipe] = useState(null);
+
+  const fetchRecipe = () => {
+    fetch(
+      "https://api.react-formula.com/learning-api/demos/using-fetch/recipes"
+    )
+      .then((response) => response.json())
+      .then((data) => setRecipe(data));
+  };
   return (
     <div className="flex flex-col items-center">
-      <RecipeCard recipe={recipeCard} />
-      <button className="bg-sky-600 hover:bg-sky-400 text-sky-50 text-2xl py-4 px-6 rounded-full">
+      {recipe && <RecipeCard recipe={recipe} />}
+      <button
+        onClick={fetchRecipe}
+        className="bg-sky-600 hover:bg-sky-400 text-sky-50 text-2xl py-4 px-6 rounded-full"
+      >
         Get Random Recipe
       </button>
     </div>
--- fetch-recipe/src/RecipeCard.jsx
+++ fetch-recipe/src/RecipeCard.jsx
@@ -1,9 +1,9 @@
 const RecipeCard = (props) => {
-  const { name, imageUrl } = props.recipe;
+  const { name, image_url } = props.recipe;
   return (
-    <div className="m-8  bg-indigo-500 text-indigo-100 text-center text-3xl rounded-xl overflow-clip">
+    <div className="m-8  bg-indigo-500 text-indigo-100 text-center text-3xl rounded-lg overflow-clip">
       <div className="p-6 ">{name}</div>
-      <img src={imageUrl} className="max-w-[450px] h-[350px] object-cover" />
+      <img src={image_url} className="w-[400px] h-[320px] object-cover" />
     </div>
   );
 };
