266fe10c183d8950e4b8204fb83fc67303d1b094
Fl3mish <mitch.pauwels@proton.me>
2024-09-09 21:11:13 +0100

    UI Build

--- fetch-recipe/src/App.jsx
+++ fetch-recipe/src/App.jsx
@@ -1,3 +1,19 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Skeleton</h1>;
-}
+import RecipeCard from "./RecipeCard";
+
+const recipeCard = {
+  name: "Hamburger and Fries",
+  imageUrl:
+    "https://images.pexels.com/photos/1556688/pexels-photo-1556688.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+};
+
+const App = () => {
+  return (
+    <div className="flex flex-col items-center">
+      <RecipeCard recipe={recipeCard} />
+      <button className="bg-sky-600 hover:bg-sky-400 text-sky-50 text-2xl py-4 px-6 rounded-full">
+        Get Random Recipe
+      </button>
+    </div>
+  );
+};
+export default App;
--- fetch-recipe/src/RecipeCard.jsx
+++ fetch-recipe/src/RecipeCard.jsx
@@ -1,4 +1,10 @@
-const RecipeCard = () => {
-  return <div>RecipeCard</div>;
+const RecipeCard = (props) => {
+  const { name, imageUrl } = props.recipe;
+  return (
+    <div className="m-8  bg-indigo-500 text-indigo-100 text-center text-3xl rounded-xl overflow-clip">
+      <div className="p-6 ">{name}</div>
+      <img src={imageUrl} className="max-w-[450px] h-[350px] object-cover" />
+    </div>
+  );
 };
 export default RecipeCard;
