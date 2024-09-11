109fd9422478e72c6b27f16c2a03efbe5a5610ad
Fl3mish <mitch.pauwels@proton.me>
2024-09-11 14:06:23 +0100

    Added Update Functionlity

--- deleting-furni/src/FurnitureItem.jsx
+++ deleting-furni/src/FurnitureItem.jsx
@@ -1,20 +1,76 @@
+import { useState } from "react";
 import * as FurnitureService from "./services/furniture";
 
 const FurnitureItem = (props) => {
   const { furniture, fetchFurniture } = props;
+  const [currentName, setCurrentName] = useState(furniture.name);
+  const [currentDescription, setCurrentDescription] = useState(
+    furniture.description
+  );
+  const [editing, setEditing] = useState(false);
   return (
     <div className="m-4 flex border border-slate-400 rounded-md overflow-clip">
       <img src={furniture.image} className="w-48 h-48" />
       <div className="w-full px-6 py-4 bg-white flex flex-col justify-between">
-        <div className="text-2xl">{furniture.name}</div>
-        <div>{furniture.description}</div>
+        {editing ? (
+          <>
+            <input
+              type="text"
+              value={currentName}
+              onChange={(e) => setCurrentName(e.target.value)}
+              className="border border-stone-200 rounded-md shadow-inner p-1 text-xl text-blue-600 my-1"
+            />
+            <textarea
+              value={currentDescription}
+              onChange={(e) => setCurrentDescription(e.target.value)}
+              className="border border-stone-200 rounded-md shadow-inner p-1 text-xl text-blue-600 my-1"
+            />
+          </>
+        ) : (
+          <>
+            {" "}
+            <div className="text-2xl">{furniture.name}</div>
+            <div>{furniture.description}</div>
+          </>
+        )}
+
         <div className=" flex justify-end">
+          {/* Edit/Save */}
+          {editing ? (
+            <button
+              onClick={async () => {
+                setEditing(false);
+
+                await FurnitureService.updateFurniture(furniture.id, {
+                  image: furniture.image,
+                  name: currentName,
+                  description: currentDescription,
+                });
+
+                fetchFurniture();
+              }}
+              className="bg-green-500 hover:bg-green-700 text-green-50 py-2 px-4 rounded-md mx-1"
+            >
+              <i className="fa-solid fa-check text-red-50 mr-1"></i>Save
+            </button>
+          ) : (
+            <button
+              onClick={() => {
+                setEditing(true);
+              }}
+              className="bg-blue-500 hover:bg-blue-700 text-blue-50 py-2 px-4 rounded-md mx-1"
+            >
+              <i className="fa-solid fa-pen-to-square text-red-50 mr-1"></i>Edit
+            </button>
+          )}
+
+          {/* Delete */}
           <button
             onClick={async () => {
               await FurnitureService.deleteFurniture(furniture.id);
               fetchFurniture();
             }}
-            className="bg-red-500 hover:bg-red-700 text-red-50 py-2 px-4 rounded-md"
+            className="mx-1 bg-red-500 hover:bg-red-700 text-red-50 py-2 px-4 rounded-md"
           >
             <i className="fa-solid fa-trash text-red-50 mr-1"></i>Delete
           </button>
--- deleting-furni/src/services/furniture.js
+++ deleting-furni/src/services/furniture.js
@@ -2,3 +2,10 @@ export const getAllFurniture = () => fetch("http://localhost:5000/furniture");
 
 export const deleteFurniture = (furnitureId) =>
   fetch(`http://localhost:5000/furniture/${furnitureId}`, { method: "DELETE" });
+
+export const updateFurniture = (furnitureId, body) =>
+  fetch(`http://localhost:5000/furniture/${furnitureId}`, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(body),
+  });
