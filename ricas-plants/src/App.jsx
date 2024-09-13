724851be980dec7bc57f95d27221910bd394933c
Fl3mish <mitch.pauwels@proton.me>
2024-09-13 12:57:30 +0100

    imported fonts + fontawesome

--- ricas-plants/src/App.jsx
+++ ricas-plants/src/App.jsx
@@ -1,9 +1,6 @@
-
 const App = () => {
   return (
-    <div className="bg-yellow-100 text-blue-500 h-screen flex justify-center items-center text-4xl">
-      hello
-    </div>
+    <div className="text-green-600 flex flex-col justify-center items-center h-screen text-4xl"></div>
   );
 };
 export default App;
