51689b3f7bd7586ed0ec198ebe519add5fa96113
Fl3mish <mitch.pauwels@proton.me>
2024-09-13 01:57:56 +0100

    init commit

---
+++ ricas-plants/src/App.jsx
@@ -0,0 +1,9 @@
+
+const App = () => {
+  return (
+    <div className="bg-yellow-100 text-blue-500 h-screen flex justify-center items-center text-4xl">
+      hello
+    </div>
+  );
+};
+export default App;
