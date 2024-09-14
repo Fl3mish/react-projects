c5e27ff65a09b15c710642233146df805b238b79
Fl3mish <mitch.pauwels@proton.me>
2024-09-14 12:59:32 +0100

    submit logic / error handling / conditional message rending wiht useLocation hook

--- ricas-plants/src/pages/auth/SignInPage.jsx
+++ ricas-plants/src/pages/auth/SignInPage.jsx
@@ -1,22 +1,43 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import AuthForm from "./AuthForm";
 import FormContainer from "./AuthForm/FormContainer";
-
+import * as userService from "services/user";
+import { useState } from "react";
 const SignInPage = () => {
+  const [error, setError] = useState("");
+  const location = useLocation();
+
+  console.log("location " + location.state);
+
   return (
     <FormContainer>
+      <div className="font-lato text-red-700">{error}</div>
+      {location.state?.accountCreated && (
+        <div className="mb-8 mt-2 rounded-lg border border-emerald-500 bg-green-200 p-4 font-lato text-emerald-700">
+          Account created successfully, Please sign in.
+        </div>
+      )}
       <AuthForm
         fields={[
-          {
-            label: "username",
-            type: "text",
-          },
-          {
-            label: "password",
-            type: "password",
-          },
+          { label: "username", type: "text" },
+          { label: "password", type: "password" },
         ]}
         submitButtonLabel="sign in"
+        onSubmit={async (values) => {
+          const response = await userService.createSession({
+            username: values.username,
+            password: values.password,
+          });
+          // Server response handling
+          if (response.status === 201) {
+            console.log("Sign in succesfull");
+            setError("");
+          } else {
+            const data = await response.json();
+            setError(data.error);
+            console.log("sign in unsuccesfull");
+          }
+        }}
       />
       <Link to={"/sign-up"} className="text-sm text-green-600 underline">
         create an account
