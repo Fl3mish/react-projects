f49839b3ced81d71ccfbe9e62f5fa9643a9a4511
Fl3mish <mitch.pauwels@proton.me>
2024-09-16 22:21:53 +0100

    Added a background image for screen larger than mobile

--- auth-form/src/pages/auth/Layout.jsx
+++ auth-form/src/pages/auth/Layout.jsx
@@ -1,17 +1,28 @@
 const Layout = (props) => {
   const { children } = props;
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-green-50">
-      <div className="flex flex-col items-center mx-2 my-8">
+    <div className="flex">
+      <div className="hidden md:flex relative">
         <img
-          src="https://d1d9816gvj4fau.cloudfront.net/capstone_logo_dark.png"
-          className="w-16 mb-2"
+          src="https://d1d9816gvj4fau.cloudfront.net/capstone_sign_in_scene.png"
+          className="h-screen object-cover"
         />
+        <div className="absolute top-0 left-0 w-full h-full bg-black/10"></div>
+        <div className="absolute top-0 left-0 w-full h-full bg-green-800/40"></div>
       </div>
-      <div className="text-3xl font-playfair text-emerald-700">
-        Rica's Plants
+
+      <div className="flex flex-col flex-1 items-center justify-center h-screen bg-green-50">
+        <div className="flex flex-col items-center mx-2 my-8">
+          <img
+            src="https://d1d9816gvj4fau.cloudfront.net/capstone_logo_dark.png"
+            className="w-16 mb-2"
+          />
+        </div>
+        <div className="text-3xl font-playfair text-emerald-700">
+          Rica's Plants
+        </div>
+        {children}
       </div>
-      {children}
     </div>
   );
 };
