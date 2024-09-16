5973d50649c4550f889b211f1989f2eba8789cd3
Fl3mish <mitch.pauwels@proton.me>
2024-09-16 19:05:44 +0100

    pages -> auth dir made with signin/signup page.

---
+++ auth-form/src/pages/auth/SignInPage.jsx
@@ -0,0 +1,8 @@
+const SignInPage = () => {
+  return (
+    <div className="bg-yellow-100 text-blue-500 h-screen flex justify-center items-center text-4xl">
+      SignInPage
+    </div>
+  );
+};
+export default SignInPage;
---
+++ auth-form/src/pages/auth/SignUpPage.jsx
@@ -0,0 +1,8 @@
+const SignUpPage = () => {
+  return (
+    <div className="bg-yellow-100 text-blue-500 h-screen flex justify-center items-center text-4xl">
+      SignUpPage
+    </div>
+  );
+};
+export default SignUpPage;
