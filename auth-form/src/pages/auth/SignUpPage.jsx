8d32a651ada74201f7431b129d949be6ebbdf2ac
Fl3mish <mitch.pauwels@proton.me>
2024-09-16 23:18:59 +0100

    validation and error handling on register

--- auth-form/src/pages/auth/SignUpPage.jsx
+++ auth-form/src/pages/auth/SignUpPage.jsx
@@ -1,10 +1,16 @@
 import { Link } from "react-router-dom";
 import Form from "./Form";
 import Layout from "./Layout";
+import { useState } from "react";
 
 const SignUpPage = () => {
+  const [error, setError] = useState("");
+
   return (
     <Layout>
+      <div className="bg-red-200 text-red-700 px-4 py-2 mt-2 rounded-lg">
+        {error.msg}
+      </div>
       <Form
         fields={[
           { label: "username", type: "string" },
@@ -13,7 +19,20 @@ const SignUpPage = () => {
         ]}
         buttonLabel="Sign Up"
         onSubmit={(values) => {
-          console.log(values);
+          if (values.username.length < 4) {
+            setError({ msg: "username too short" });
+            return;
+          }
+          if (values.password.length < 4) {
+            setError({ msg: "password too short" });
+            return;
+          }
+          if (values.password !== values.confirmPassword) {
+            setError({ msg: "passwords do not match" });
+            return;
+          }
+
+          //   Make Request
         }}
       />
       <Link to={"/sign-up"} className="text-sm text-green-600 underline">
