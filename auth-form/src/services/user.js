4d6279bb31d2d4e7853474b19f57ba56e23825df
Fl3mish <mitch.pauwels@proton.me>
2024-09-17 13:07:51 +0100

    created functions to handle the jwt token

--- auth-form/src/services/user.js
+++ auth-form/src/services/user.js
@@ -5,3 +5,13 @@ export const createUser = ({ username, password }) =>
 
 export const createSession = ({ username, password }) =>
   apiFetch("POST", "/users/session", { username, password });
+
+// Set token in local storage -> logged in
+export const setTokenSessionStorage = (token) =>
+  localStorage.setItem("token", token);
+
+// Get token from local storage -> Logged in
+export const getTokenSessionStorage = () => localStorage.getItem("token");
+
+// Delete token from local storage -> Signed out
+export const removeTokenSessionStorage = () => localStorage.removeItem("token");
