568c036a77340471d8a6c2415a774f23c3ed52e1
Fl3mish <mitch.pauwels@proton.me>
2024-09-17 15:45:36 +0100

    implemented redirect o to signup if logged in

--- auth-form/src/pages/auth/SignUpPage.jsx
+++ auth-form/src/pages/auth/SignUpPage.jsx
@@ -3,6 +3,7 @@ import Form from "./Form";
 import Layout from "./Layout";
 import { useState } from "react";
 import { createUser } from "services/user";
+import Redirect from "shared/Redirect";
 
 const SignUpPage = () => {
   const [error, setError] = useState("");
@@ -10,56 +11,58 @@ const SignUpPage = () => {
   const navigate = useNavigate();
 
   return (
-    <Layout>
-      {error && (
-        <div className="bg-red-200 text-red-700 px-4 py-2 mt-2 rounded-lg  duration-100">
-          {error.msg}
-        </div>
-      )}
+    <Redirect>
+      <Layout>
+        {error && (
+          <div className="bg-red-200 text-red-700 px-4 py-2 mt-2 rounded-lg  duration-100">
+            {error.msg}
+          </div>
+        )}
 
-      <Form
-        fields={[
-          { label: "username", type: "string" },
-          { label: "password", type: "password" },
-          { label: "confirmPassword", type: "password" },
-        ]}
-        buttonLabel="Sign Up"
-        onSubmit={async (values) => {
-          if (values.username.length < 4) {
-            setError({ msg: "username too short" });
-            return;
-          }
-          if (values.password.length < 4) {
-            setError({ msg: "password too short" });
-            return;
-          }
-          if (values.password !== values.confirmPassword) {
-            setError({ msg: "passwords do not match" });
-            return;
-          }
+        <Form
+          fields={[
+            { label: "username", type: "string" },
+            { label: "password", type: "password" },
+            { label: "confirmPassword", type: "password" },
+          ]}
+          buttonLabel="Sign Up"
+          onSubmit={async (values) => {
+            if (values.username.length < 4) {
+              setError({ msg: "username too short" });
+              return;
+            }
+            if (values.password.length < 4) {
+              setError({ msg: "password too short" });
+              return;
+            }
+            if (values.password !== values.confirmPassword) {
+              setError({ msg: "passwords do not match" });
+              return;
+            }
 
-          //   Make Request
-          const response = await createUser({
-            username: values.username,
-            password: values.password,
-          });
-          console.log(response);
-          if (response.status === 201) {
-            setError("");
-            navigate("/", { state: { accountCreated: true } });
-            console.log("user created");
-            return;
-          } else {
-            const data = await response.json();
-            setError(data);
-            return;
-          }
-        }}
-      />
-      <Link to={"/"} className="text-sm text-green-600 underline">
-        sign in
-      </Link>
-    </Layout>
+            //   Make Request
+            const response = await createUser({
+              username: values.username,
+              password: values.password,
+            });
+            console.log(response);
+            if (response.status === 201) {
+              setError("");
+              navigate("/", { state: { accountCreated: true } });
+              console.log("user created");
+              return;
+            } else {
+              const data = await response.json();
+              setError(data);
+              return;
+            }
+          }}
+        />
+        <Link to={"/"} className="text-sm text-green-600 underline">
+          sign in
+        </Link>
+      </Layout>
+    </Redirect>
   );
 };
 export default SignUpPage;
